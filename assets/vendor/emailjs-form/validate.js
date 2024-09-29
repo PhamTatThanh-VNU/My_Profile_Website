809d49c7574e66aa531f668cb225c7b5bbb812f5
PhamTatThanh-VNU <thanhpham2k4nz@gmail.com>
2024-09-29 13:31:55 +0700

    sent email feature

--- assets/vendor/emailjs-form/validate.js
+++ assets/vendor/emailjs-form/validate.js
@@ -10,11 +10,12 @@ window.onload = function () {
     .getElementById("contact-form")
     .addEventListener("submit", function (event) {
       event.preventDefault();
-
+      document.getElementById("contact-form").reset();
       emailjs.sendForm("service_79yk9qn", "template_pwdvzdi", this).then(
         function (response) {
           const successAlert = document.getElementById("success-alert");
           successAlert.style.display = "block"; // Show the alert
+
           setTimeout(() => {
             successAlert.style.display = "none"; // Hide after 5 seconds
           }, 3000);
@@ -22,6 +23,9 @@ window.onload = function () {
         function (error) {
           const errorAlert = document.getElementById("error-alert");
           errorAlert.style.display = "block"; // Show the alert
+
+          document.getElementById("contact-form").reset();
+
           setTimeout(() => {
             errorAlert.style.display = "none"; // Hide after 5 seconds
           }, 3000);
