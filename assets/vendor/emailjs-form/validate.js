b0474070bbc7257a312d01042f059102e3a52b0b
PhamTatThanh-VNU <thanhpham2k4nz@gmail.com>
2024-09-29 13:23:30 +0700

    done send email function

---
+++ assets/vendor/emailjs-form/validate.js
@@ -0,0 +1,31 @@
+(function () {
+  emailjs.init({
+    publicKey: "epDiH0_GUFGsmGkEj",
+  });
+})();
+window.onload = function () {
+  "use strict";
+
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      emailjs.sendForm("service_79yk9qn", "template_pwdvzdi", this).then(
+        function (response) {
+          const successAlert = document.getElementById("success-alert");
+          successAlert.style.display = "block"; // Show the alert
+          setTimeout(() => {
+            successAlert.style.display = "none"; // Hide after 5 seconds
+          }, 3000);
+        },
+        function (error) {
+          const errorAlert = document.getElementById("error-alert");
+          errorAlert.style.display = "block"; // Show the alert
+          setTimeout(() => {
+            errorAlert.style.display = "none"; // Hide after 5 seconds
+          }, 3000);
+        }
+      );
+    });
+};
